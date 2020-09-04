378f375e3411491cde10a2f631d561c82f5cc46d
CampL <58517030+CampLSNIR@users.noreply.github.com>
2020-09-04 17:06:22 +0200

    Update question5.cpp

--- revision/question5.cpp
+++ revision/question5.cpp
@@ -10,7 +10,15 @@ int main() {
 	std::cout << "Entrer le prix hors taxe\n";
 	std::cin >> prix;
 
-	std::cout << "Prix hors taxe : " << prix << "\nTVA : 20%\n" << "Prix : " << prix*1.2;
+	int quantite;
+	std::cout << "Entrer la quantite\n";
+	std::cin >> quantite;
+
+	float tva;
+	std::cout << "Entrer la tva en %\n";
+	std::cin >> tva;
+
+	std::cout << "Prix hors taxe : " << prix << "\nTVA : "<< tva <<"\n" << "Prix : " << (prix*quantite)*(tva/100+1);
 
 	return 0;
 
