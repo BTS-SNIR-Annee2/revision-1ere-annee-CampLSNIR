a96cb24cd0b9f245890b719be70dcec52abb392c
CampL <58517030+CampLSNIR@users.noreply.github.com>
2020-09-04 15:35:16 +0200

    question 8

---
+++ revision/question8.cpp
@@ -0,0 +1,23 @@
+
+
+#include<iostream>
+#include<string>
+
+int main() {
+
+	int nbre;
+	std::cout << "Entrer un nombre\n";
+	std::cin >> nbre;
+
+
+	if (nbre == 0) {
+		std::cout << nbre << " est egal a 0 \n";
+	}else if (nbre > 0) {
+		std::cout << nbre << " est positif \n";
+	}else {
+		std::cout << nbre << " est negatif \n";
+	}
+
+	return 0;
+
+}
