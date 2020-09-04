34d4bb77e5f124a7c36770da7bba19b46adc580b
CampL <58517030+CampLSNIR@users.noreply.github.com>
2020-09-04 14:56:50 +0200

    exercice 1

--- revision/main.cpp
+++ revision/main.cpp
@@ -5,10 +5,29 @@
 
 
 
+void ex1() {
+
+    int a = 5;
+    int b = 6;
+
+    printf("a = %d ; b = %d \n", a , b );
+
+    int tmp = a;
+    a = b;
+    b = tmp;
+
+    printf("a = %d ; b = %d \n", a, b);
+
+}
+
+void ex2() {
+
+
+}
 
 int main(){
     
-    
+    ex1();
 
     
 }
