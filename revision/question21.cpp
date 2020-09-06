8d7125384ee4c0d482495beb8d195ba2a9a93058
CampLSNIR <camp.leonardo@lyceeppr.fr>
2020-09-06 18:35:45 +0200

    q21

---
+++ revision/question21.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+
+int main() {
+
+	int tab[12][8] = {};
+
+	int max = 0;
+	for (size_t i = 0; i < 12; i++)
+	{
+		for (size_t k = 0; k < 8; k++) {
+			tab[i][k] = rand();
+
+			if (i == 0 and k == 0) {
+				max = tab[i][k];
+			}else if(tab[i][k] > max){
+				max = tab[i][k];
+			}
+		}
+	}
+
+	std::cout << "le max est " << max << "\n";
+
+}
